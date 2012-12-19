71c6c781e3635323fb9fde69b5609eb24b135939
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2012-12-19 11:41:08 -0800

    Ignoring duplicate entries

--- app/code/local/Aoe/VarnishAsyncCache/Helper/Data.php
+++ app/code/local/Aoe/VarnishAsyncCache/Helper/Data.php
@@ -2,21 +2,31 @@
 
 class Aoe_VarnishAsyncCache_Helper_Data extends Magneto_Varnish_Helper_Data {
 
-    /**
-     * Store an array of urls in cache for purging with Aoe_AsyncCache extension later
-     *
-     * @param   array   $urls
-     * @return  void
-     */
+	/**
+	 * Store an array of urls in cache for purging with Aoe_AsyncCache extension later
+	 *
+	 * @param   array   $urls
+	 * @throws  Zend_Db_Statement_Exception
+	 * @return  void
+	 */
     public function purge(array $urls) {
 
-        foreach ($urls as $url) {
-            $asynccache = Mage::getModel('aoeasynccache/asynccache');
+        foreach ($urls as $url) { /* @var $url string */
+            $asynccache = Mage::getModel('aoeasynccache/asynccache'); /* @var $asynccache Aoe_AsyncCache_Model_Asynccache */
             $asynccache->setTstamp(time());
             $asynccache->setMode('purgeVarnishUrl');
             $asynccache->setTags($url);
             $asynccache->setStatus('pending');
-            $asynccache->save();
+
+            try {
+                $asynccache->save();
+            } catch (Zend_Db_Statement_Exception $e) {
+                if ($e->getCode() != 23000) { // Integrity constraint violation: 1062 Duplicate entry '...' for key 'IDX_ASYNCCACHE_MODE_TAGS_STATUS'
+                    throw $e;
+                }
+                // otherwise ignore duplicate entry...
+            }
+
         }
     }
 
